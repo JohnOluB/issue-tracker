90752bea79176d3561bbb67db09d4fd1ca38e7c7
JohnOluB <adebayodeolu90@gmail.com>
2026-01-09 18:00:30 +0100

    fix: dark theme functionality

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const themeBtn = document.querySelector("#theme-button");
-const mainContainer = document.querySelector(".main-container");
 
 
 function changeTheme() {
-    mainContainer.classList.toggle('dark-theme');
-    if (mainContainer.classList.contains('dark-theme')) {
-        themeBtn.textContent = "Light mode";
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        themeBtn.textContent = "Light Mode";
     } else {
-        themeBtn.textContent = "Dark mode";
+        themeBtn.textContent = "Dark Mode";
     }
 }
 themeBtn.addEventListener('click', changeTheme);
